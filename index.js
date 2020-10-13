c4ee4f6d8b8860ecd468ecd265c6e7d80e12072e
Talisman000 <s.k.recode226@gmail.com>
2020-10-14 01:35:02 +0900

    fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-'use strict';
-
-module.exports = {
+"use strict";
+function isMultipleOfSeventeen(number) {
+  if (number % 17 === 0) {
+    return true;
+  }
+  return false;
 }
+module.exports = { isMultipleOfSeventeen };
